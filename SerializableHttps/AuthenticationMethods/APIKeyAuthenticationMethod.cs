38a41a96e94966431aff6d519827098bfd73801d
Kristian Skov <kris701kj@gmail.com>
2024-08-28 09:53:01 +0200

    Added an option to authenticate with an API key

---
+++ SerializableHttps/AuthenticationMethods/APIKeyAuthenticationMethod.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+namespace SerializableHttps.AuthenticationMethods
+{
+	public class APIKeyAuthenticationMethod : IAuthenticationMethod
+	{
+		public string Key { get; set; }
+
+		public APIKeyAuthenticationMethod(string key)
+		{
+			Key = key;
+		}
+
+		public AuthenticationHeaderValue GetHeaderValue() => new AuthenticationHeaderValue("Api-Key", Key);
+	}
+}
