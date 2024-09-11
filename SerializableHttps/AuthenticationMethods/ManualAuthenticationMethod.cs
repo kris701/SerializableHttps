564b1096b8cd184d63eb9099a49d361d454aecfd
Kristian Skov <kris701kj@gmail.com>
2024-09-11 07:26:55 +0200

    Added an option for a manual authentication header

---
+++ SerializableHttps/AuthenticationMethods/ManualAuthenticationMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableHttps.AuthenticationMethods
+{
+	public class ManualAuthenticationMethod : IAuthenticationMethod
+	{
+		public AuthenticationHeaderValue Header { get; set; }
+
+		public ManualAuthenticationMethod(AuthenticationHeaderValue header)
+		{
+			Header = header;
+		}
+
+		public AuthenticationHeaderValue GetHeaderValue() => Header;
+	}
+}
